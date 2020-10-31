9977c45cb8d24171d9f5a88037cd7624f9f970c8
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-31 13:55:39 +0530

    Experience
     Changes to be committed:
    	new file:   1.16/src/main/java/io/github/clothcreators/arcanecraft/block/CrystalOreBlock.java
    	modified:   1.16/src/main/java/io/github/clothcreators/arcanecraft/block/ModBlocks.java

---
+++ 1.16/src/main/java/io/github/clothcreators/arcanecraft/block/CrystalOreBlock.java
@@ -0,0 +1,23 @@
+package io.github.clothcreators.arcanecraft.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+
+public class CrystalOreBlock extends Block {
+	public CrystalOreBlock(Settings settings) {
+		super(settings);
+	}
+
+	@Override
+	public void onStacksDropped(BlockState state, ServerWorld world, BlockPos pos, ItemStack stack) {
+		super.onStacksDropped(state, world, pos, stack);
+		if (EnchantmentHelper.getLevel(Enchantments.SILK_TOUCH, stack) == 0) {
+			this.dropExperience(world, pos, 2);
+		}
+	}
+}
--- 1.16/src/main/java/io/github/clothcreators/arcanecraft/block/ModBlocks.java
+++ 1.16/src/main/java/io/github/clothcreators/arcanecraft/block/ModBlocks.java
@@ -2,12 +2,10 @@ package io.github.clothcreators.arcanecraft.block;
 
 import static io.github.clothcreators.arcanecraft.ArcaneCraft3.id;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
 import net.minecraft.block.SlabBlock;
-import net.minecraft.block.StairsBlock;
 import net.minecraft.block.WallBlock;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.registry.Registry;
@@ -20,7 +18,7 @@ public class ModBlocks {
 	}
 
 	public static final Block RUNIC_STONE = register("runic_stone", new Block(FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.STONE).strength(1.1F, 17.5F).breakByHand(false).breakByTool(FabricToolTags.PICKAXES, 0)));
-	public static final Block CRYSTAL_ORE = register("crystal_ore", new Block(FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.METAL).strength(2.0F, 17.5F).breakByHand(false).breakByTool(FabricToolTags.PICKAXES, 2)));
+	public static final Block CRYSTAL_ORE = register("crystal_ore", new CrystalOreBlock(FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.METAL).strength(2.0F, 17.5F).breakByHand(false).breakByTool(FabricToolTags.PICKAXES, 2)));
 	public static final Block RUNIC_STONE_STAIRS = register("runic_stone_stairs", new StairsBlock(RUNIC_STONE.getDefaultState(), FabricBlockSettings.copyOf(RUNIC_STONE)));
 	public static final Block RUNIC_STONE_SLAB = register("runic_stone_slab", new SlabBlock(FabricBlockSettings.copyOf(RUNIC_STONE)));
 	public static final Block RUNIC_STONE_WALL = register("runic_stone_wall", new WallBlock(FabricBlockSettings.copyOf(RUNIC_STONE)));
