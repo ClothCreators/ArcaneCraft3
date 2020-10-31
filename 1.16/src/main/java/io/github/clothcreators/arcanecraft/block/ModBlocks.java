7f25abce6a66bb51cfd8ef15d5dc1f78114a8b21
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-31 10:31:09 +0530

    Runic stone

--- 1.16/src/main/java/io/github/clothcreators/arcanecraft/block/ModBlocks.java
+++ 1.16/src/main/java/io/github/clothcreators/arcanecraft/block/ModBlocks.java
@@ -2,12 +2,20 @@ package io.github.clothcreators.arcanecraft.block;
 
 import static io.github.clothcreators.arcanecraft.ArcaneCraft3.id;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.registry.Registry;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 
 public class ModBlocks {
 	private ModBlocks() {
 	}
+
+	public static final Block RUNIC_STONE = register("runic_stone", new Block(FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.STONE).strength(1.1F, 17.5F).breakByHand(false).breakByTool(FabricToolTags.PICKAXES, 0)));
 
 	private static Block register(String name, Block block) {
 		return Registry.register(Registry.BLOCK, id(name), block);
--- 1.16/src/main/java/io/github/clothcreators/arcanecraft/item/ModItems.java
+++ 1.16/src/main/java/io/github/clothcreators/arcanecraft/item/ModItems.java
@@ -2,14 +2,23 @@ package io.github.clothcreators.arcanecraft.item;
 
 import static io.github.clothcreators.arcanecraft.ArcaneCraft3.id;
 
+import io.github.clothcreators.arcanecraft.block.ModBlocks;
+
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.registry.Registry;
+
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 
 public class ModItems {
 	private ModItems() {
 	}
+
+	private static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.create(id("item_group")).icon(() -> null).build();
+	public static final Item RUNIC_STONE = register("runic_stone", ModBlocks.RUNIC_STONE, new FabricItemSettings().group(ITEM_GROUP));
 
 	private static Item register(String name, Block block, Item.Settings settings) {
 		return Registry.register(Registry.ITEM, id(name), new BlockItem(block, settings));
