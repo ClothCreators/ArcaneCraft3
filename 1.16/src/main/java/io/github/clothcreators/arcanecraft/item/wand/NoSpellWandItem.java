28e88d302058b2403e3611aecc767f30299fb598
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-31 21:25:39 +0530

    Tags
     Changes to be committed:
    	modified:   1.16/src/main/java/io/github/clothcreators/arcanecraft/item/wand/NoSpellWandItem.java

--- 1.16/src/main/java/io/github/clothcreators/arcanecraft/item/wand/NoSpellWandItem.java
+++ 1.16/src/main/java/io/github/clothcreators/arcanecraft/item/wand/NoSpellWandItem.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
@@ -20,5 +22,18 @@ public class NoSpellWandItem extends AbstractWandItem {
 	public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
 		super.appendTooltip(stack, world, tooltip, context);
 		tooltip.add(new TranslatableText("tooltip.arcanecraft3.no_spell").formatted(Formatting.DARK_GRAY));
+	}
+
+	@Override
+	public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+		super.inventoryTick(stack, world, entity, slot, selected);
+		if (!stack.getOrCreateTag().contains("Slots")) {
+			CompoundTag slots = new CompoundTag();
+			slots.putString("Slot1", "[No Spell]");
+			slots.putString("Slot2", "[No Spell]");
+			slots.putString("Slot3", "[No Spell]");
+			slots.putString("Slot4", "[No Spell]");
+			stack.getOrCreateTag().put("Slots", slots);
+		}
 	}
 }
