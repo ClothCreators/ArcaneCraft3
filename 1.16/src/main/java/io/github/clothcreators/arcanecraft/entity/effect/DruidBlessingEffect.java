47abbd1fd95bc265a238cd7836b6dad2ea13e666
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-31 12:25:13 +0530

    Translationkeys

--- 1.16/src/main/java/io/github/clothcreators/arcanecraft/entity/effect/DruidBlessingEffect.java
+++ 1.16/src/main/java/io/github/clothcreators/arcanecraft/entity/effect/DruidBlessingEffect.java
@@ -1,5 +1,7 @@
 package io.github.clothcreators.arcanecraft.entity.effect;
 
+import io.github.clothcreators.arcanecraft.ArcaneCraft3;
+
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectType;
 
@@ -11,6 +13,11 @@ public class DruidBlessingEffect extends StatusEffect {
 	@Override
 	public boolean isInstant() {
 		return false;
+	}
+
+	@Override
+	public String getTranslationKey() {
+		return ArcaneCraft3.MOD_ID + "statuseffect.druid_blessing";
 	}
 
 	@Override
